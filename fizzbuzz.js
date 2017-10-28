bffc08e532c131927db4c811fb6a7eaec6da1b23
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-10-28 01:32:52 +0100

    Workshop solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,13 @@
 function fizzbuzz(num) {
-
+	if (num % 3 !== 0 && num % 5 !== 0) {
+		return num;
+	} else if (num % 3 === 0 && num % 5 !== 0) {
+		return "Fizz";
+	} else if (num % 5 === 0 && num % 3 !== 0) {
+		return "Bazz";
+	} else {
+		return "FizzBazz";
+	}
 }
 
 module.exports = fizzbuzz;
