52c1c2ce9fb9257ea3cc6913596f1b08d292f83f
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-10-28 01:24:43 +0100

    Add readme

---
+++ fizzbuzz.js
@@ -0,0 +1,5 @@
+function fizzbuzz(num) {
+
+}
+
+module.exports = fizzbuzz;
---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,7 @@
+const test = require("tape");
+const fizzbuzz = require("../fizzbuzz");
+
+test("tape is working", function(t){
+	t.equals(1, 1, "one equals one");
+	t.end();
+});
